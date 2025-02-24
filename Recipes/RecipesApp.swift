de87727fd1a42f533578831f6a24c45ed87aaae5
Brayden Harris <brayden4012@gmail.com>
2025-02-24 14:09:41 -0600

    fix RecipesApp file reference

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Brayden Harris on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipesListView()
+        }
+    }
+}
